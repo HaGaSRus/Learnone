0687d4ab5a1473e66b2e0fd26723e2d7139fcaa1
George Trofimov <tro37295@gmail.com>
2024-03-14 14:29:33 +0500

    Добавление заголовка страницы

--- modules/job/views.py
+++ modules/job/views.py
@@ -8,3 +8,7 @@ class ArticleListView(ListView):
     template_name = 'blog/articles_list.html'
     context_object_name = 'articles'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
