51d89cdd08deabeff2889dd0ee97e4f656d97f87
George Trofimov <tro37295@gmail.com>
2024-03-14 14:15:52 +0500

    Создали url для вывода базовой модели статьи

--- modules/job/views.py
+++ modules/job/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 from .models import Article
 
+
 def article_list(request):
     articles = Article.objects.all()
-    return render(request, 'blog/article_list.html', {'articles': articles})
-    
+    return render(request, 'blog/articles_list.html', {'articles': articles})
