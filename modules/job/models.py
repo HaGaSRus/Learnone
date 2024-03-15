b3a79ee049e7e29fa09c79207c80d2c008e340dd
George Trofimov <tro37295@gmail.com>
2024-03-15 14:29:54 +0500

    Оптимизировали колличество SQL запросов путем применения select_related() и создали приложение system

--- modules/job/models.py
+++ modules/job/models.py
@@ -24,7 +24,7 @@ class Article(models.Model):
             """
             Список статей (SQL запрос с фильтрацией для страницы списка статей)
             """
-            return self.get_queryset().filter(status='published')
+            return self.get_queryset().select_related('author', 'category').filter(status='published')
 
     STATUS_OPTIONS = (
         ('published', 'Опубликовано'),
