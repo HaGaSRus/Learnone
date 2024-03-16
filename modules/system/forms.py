2eeef0977393db21fe83ba065b508990e11830ff
George Trofimov <tro37295@gmail.com>
2024-03-16 15:33:14 +0500

    Создание форм для редактирования профиля

--- modules/system/forms.py
+++ modules/system/forms.py
@@ -1,12 +1,55 @@
-# class UserForm(forms.ModelForm):
-#     class Meta:
-#         model = User
-#         fields = ('first_name', 'last_name', 'email')
-#
-#
-# class ProfileForm(forms.ModelForm):
-#     class Meta:
-#         model = Profile
-#         fields = ('avatar', 'bio', 'birth_date')
-#
-#
+from django import forms
+from django.contrib.auth.models import User
+
+from .models import Profile
+
+
+class UserUpdateForm(forms.ModelForm):
+    """
+    Форма обновления данных пользователя
+    """
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Обновление стилей формы под bootstrap
+        """
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control',
+                'autocomplete': 'off'
+            })
+
+    def clean_email(self):
+        """
+        Проверка на уникальность email
+        """
+        email = self.cleaned_data.get('email')
+        username = self.cleaned_data.get('username')
+        if email and User.objects.filter(email=email).exclude(username=username).exists():
+            raise forms.ValidationError('Такое email адрес уже используется')
+        return email
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    """
+    Форма обновления данных профиля пользователя
+    """
+    class Meta:
+        model = Profile
+        fields = ('slug', 'birth_date', 'bio', 'avatar')
+
+    def __init__(self, *args, **kwargs):
+        """
+        Обновление стилей формы обновления
+        """
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control',
+                'autocomplete': 'off'
+            })
