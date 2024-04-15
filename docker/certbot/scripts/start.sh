0876e691ba67228cc8b231deaae7170b44b6350d
George Trofimov <tro37295@gmail.com>
2024-04-15 12:00:06 +0500

    Delete docker/certbot directory

--- docker/certbot/scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export WEBROOT_PATH=/var/www/certbot/
-export EMAIL=tro37295@gmail.com
-export DOMAIN=learnone.ru
-
-certbot certonly --webroot --webroot-path=$WEBROOT_PATH --email "$EMAIL" --agree-tos --no-eff-email -d "$DOMAIN" -d "www.$DOMAIN"
-~                                                                                                                           ~                                                                                                                           ~              
